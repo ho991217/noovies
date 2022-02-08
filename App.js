eb93cd0b341d651998bf696b7beddea5c47eb9a5
ho991217 <ho991217@naver.com>
2022-02-09 02:19:41 +0900

    Tab Nav

--- App.js
+++ App.js
@@ -1,6 +1,14 @@
 import AppLoading from "expo-app-loading";
-import React from "react";
+import React, { useState } from "react";
+import { Image, Text } from "react-native";
+import * as Font from "expo-font";
+import { Ionicons } from "@expo/vector-icons";
+import { useAsset } from "expo-asset";
 
 export default function App() {
-  return null;
+  const [fonts] = Font.useFonts(Ionicons.font);
+  if (!fonts) {
+    return <AppLoading />;
+  }
+  return <Text>hiadfgdsgsgf</Text>;
 }
